1b9a16aad4462e9ddadc831fb8c701f141bb1006
patodekaz <pato@dekaz.cl>
2020-04-13 22:34:09 -0400

    Updates

---
+++ precache-manifest.b5fb10d09c14cd520207c5279165c5e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965d76784e78491e15499c01dc074d9c",
+    "url": "/todolistapi/index.html"
+  },
+  {
+    "revision": "df1c6694f62b1ab962bb",
+    "url": "/todolistapi/static/css/main.66c4b30e.chunk.css"
+  },
+  {
+    "revision": "93dd784f0d868c71aafd",
+    "url": "/todolistapi/static/js/2.9089a9e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolistapi/static/js/2.9089a9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1c6694f62b1ab962bb",
+    "url": "/todolistapi/static/js/main.69d6259f.chunk.js"
+  },
+  {
+    "revision": "7d6cdb648da941177f9e",
+    "url": "/todolistapi/static/js/runtime-main.3fc0f345.js"
+  }
+]);
